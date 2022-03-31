1a714a6d143c914f2771fb7c830168c4943de682
Adva <adva.alkalay@gmail.com>
2022-03-31 16:47:43 +0300

    client footer update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,10 @@
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import { Container, Box } from "@mui/material";
+import {
+  GitHubIcon,
+  FacebookIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from "@mui/icons-material";
 
 export default function Footer() {
   return (
@@ -15,7 +14,7 @@ export default function Footer() {
         py={{ xs: 5, sm: 3 }}
         bgcolor="text.secondary"
         color="white"
-        style={{position: "relative", bottom: "0", width: "100%"}}
+        style={{ position: "relative", bottom: "0", width: "100%" }}
       >
         <Container maxWidth="lg">
           <Box textAlign="center">
